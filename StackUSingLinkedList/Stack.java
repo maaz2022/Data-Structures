ffe152f388e981882e9261c0f81d6d3934e0c578
Maaz <104203096+maaz2022@users.noreply.github.com>
2023-10-04 07:54:07 -0700

    Stack

---
+++ StackUSingLinkedList/Stack.java
@@ -0,0 +1,53 @@
+    package StackUSingLinkedList;
+
+    public class Stack {
+        class Node{
+            int data;
+            Node next;
+            public Node(int data){
+                this.data=data;
+                this.next=null;
+            }
+        }
+            Node head;
+            public boolean isEmpty(){
+                return head == null;
+            }
+            public void push(int data){
+                Node newNode = new Node(data);
+                if(isEmpty()){
+                    head = newNode;
+                    return;
+                }
+                newNode.next= head;
+                head= newNode;
+            }
+            public int pop(){
+                if(isEmpty()){
+                    return -1;
+                }
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+            public int peek(){
+            if(isEmpty()){
+                return -1;
+                }
+                return head.data;
+            }
+     
+        public static void main(String[] args) {
+        Stack s = new Stack();
+            s.push(1);
+            s.push(2);
+            s.push(3);
+            while(!s.isEmpty()){
+                System.out.println(s.peek());
+                s.pop();
+            }
+        }
+    }
+
+        
+
